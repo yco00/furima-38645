f5a750abebc7f5dd90b8e8266e303c0dde11eece
yco00 <ingo.y.co@gmail.com>
2023-03-31 08:19:12 +0900

    新規登録バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
  validates :nickname, presence: true
- validates :firstname, presence: true, format: /\A[ァ-ヶー－]+\z/
- validates :lastname, presence: true, format: /\A[ァ-ヶー－]+\z/
+ validates :firstname, presence: true, format: /\A[ぁ-んァ-ン一-龥]+\z/
+ validates :lastname, presence: true, format: /\A[ぁ-んァ-ン一-龥]+\z/
  validates :firstnameruby, presence: true, format: /\A[ァ-ヶー－]+\z/
  validates :lastnameruby, presence: true, format: /\A[ァ-ヶー－]+\z/
  validates :birthday, presence: true
+
+ VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+ validates :password, format: { with: VALID_PASSWORD_REGEX }
 
 has_many :items
 has_many :comments
