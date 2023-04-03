cda039825cdfb4be5ee722a111a819b249082a26
yco00 <ingo.y.co@gmail.com>
2023-04-03 22:58:45 +0900

    実装後のコメント削除、バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
  validates :nickname, presence: true
- validates :firstname, presence: true, format: /\A[ぁ-んァ-ン一-龥]+\z/
- validates :lastname, presence: true, format: /\A[ぁ-んァ-ン一-龥]+\z/
+ validates :firstname, presence: true, format: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+ validates :lastname, presence: true, format:  /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
  validates :firstnameruby, presence: true, format: /\A[ァ-ヶー－]+\z/
  validates :lastnameruby, presence: true, format: /\A[ァ-ヶー－]+\z/
  validates :birthday, presence: true
 
  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
  validates :password, format: { with: VALID_PASSWORD_REGEX }
-
-has_many :items
-has_many :comments
-has_many :buys
 
 end
