5e3c18050adf7659189879444e176feb1edcd401
yco00 <ingo.y.co@gmail.com>
2023-04-09 16:43:00 +0900

    エラーハンドリング修正、priceのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :title, presence: true
+  validates :descripition, presence: true
+  validates :price,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+             format: { with: /\A[0-9]+\z/ }
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -13,19 +18,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :shippingday
 
-  validates :category_id, numericality: { other_than: 1 } 
+
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-
-  validates :condition_id, numericality: { other_than: 1 } 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  
-  validates :postage_id, numericality: { other_than: 1 } 
   validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
-  
-  validates :area_id, numericality: { other_than: 1 } 
   validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
-
-  validates :shippingday_id, numericality: { other_than: 1 } 
   validates :shippingday_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
