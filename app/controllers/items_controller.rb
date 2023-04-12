4aa3432155ea84ec72a48e31e90b5d5e54e88e8f
yco00 <ingo.y.co@gmail.com>
2023-04-12 21:16:40 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item, only: [ :show, :edit, :update]
+  before_action :set_item, only: [ :show, :edit, :update, :destroy]
 
 def index
   @items=Item.order('created_at DESC')
@@ -19,7 +19,6 @@ def create
     end
 end
 
-
 def show
 end
 
@@ -35,7 +34,11 @@ def update
   end  
 end
 
-
+def destroy
+  redirect_to root_path unless current_user.id == @item.user_id
+  @item.destroy
+  redirect_to root_path
+end
 
 
 private
