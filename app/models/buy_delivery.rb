97596f205e193d0f4012a0bc828c83a382c9466b
yco00 <ingo.y.co@gmail.com>
2023-04-16 22:11:36 +0900

    buy_deliveryモデル、エラーメッセージ記述

--- app/models/buy_delivery.rb
+++ app/models/buy_delivery.rb
@@ -4,22 +4,17 @@ class BuyDelivery
 
 
   with_options presence: true do
-    validates :postno , format: { with: /\A[0-9]{3}[-][0-9]{4}\z/ }
+    validates :postno , format: { with: /\A[0-9]{3}[-][0-9]{4}\z/,message: "is invalid. Include hyphen(-)"}
+    validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :cities
     validates :address
-    validates :tel, format: { with: /\A[0-9]{10,11}\z/}
-    validates :area_id, numericality: { other_than: 0, message: 'Select' }
-    # フォームオブジェクトクラスではアソシエーションを定義できないため、Shippingモデルのbelongs_toと同じバリデーションが必要
-
+    validates :tel
   end
-
-
-
+    validates :tel, length: { minimum: 10, maximum: 12, message: 'number is too short' }
+    validates :tel,numericality: {only_integer: true, message: 'number is only number'}
+  
   def save
-    # 寄付情報を保存し、変数donationに代入する
     buy = Buy.create(item_id:item_id, user_id: user_id)
-    # 住所を保存する
-    # donation_idには、変数donationのidと指定する
     Delivery.create(postno: postno, area_id: area_id, cities: cities, address:address, building: building, tel: tel, buy_id: buy.id)
   end
 
